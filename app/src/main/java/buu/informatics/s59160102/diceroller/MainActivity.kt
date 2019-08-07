8f0e29ff8a97ed1df744defba927d1e563d7c66a
student <>
2019-08-07 18:14:45 +0700

    third

--- app/src/main/java/buu/informatics/s59160102/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160102/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package buu.informatics.s59160102.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -21,8 +22,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-        Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
